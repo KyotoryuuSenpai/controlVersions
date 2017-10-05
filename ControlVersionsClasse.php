15acabdb684a515886f9038ff5da656fe97bd12c
sergi <sergi@DESKTOP-7M7RV96>
2017-10-05 19:22:38 +0200

    afegit metode 1

---
+++ ControlVersionsClasse.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ControlVersionsClasse
+ *
+ * @author sergi
+ */
+class ControlVersionsClasse {
+    //put your code here
+    function __construct() {
+        echo "Hola esto es un control de version.";
+    }
+
+    function metode1(){
+        
+    }
+}
